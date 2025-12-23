49637bee2a72d66260c8c45bb78b6a2018b98a76
HarisShahnawaz <harisshahnawaz97@gmail.com>
2025-12-23 23:31:06 +0500

    update

--- customReact/customreact.js
+++ customReact/customreact.js
@@ -1,10 +1,23 @@
 function customRendor(reactElement,containor){
-    const domElement = document.createElement(reactElement.type)
-    domElement.innerHTML = reactElement.children
-    domElement.setAttribute('href' , reactElement.props.href)
-    domElement.setAttribute('target' , reactElement.props.target)
+
+// {
+//     const domElement = document.createElement(reactElement.type)
+//     domElement.innerHTML = reactElement.children
+//     domElement.setAttribute('href' , reactElement.props.href)
+//     domElement.setAttribute('target' , reactElement.props.target)
     
-    containor.appendChild(domElement)
+//     containor.appendChild(domElement)
+//}
+
+
+  const domElement =  document.createElement(reactElement.type)
+  domElement.innerHTML = reactElement.children
+  for (const prop in reactElement.props) {
+   if(prop === 'children') continue;
+   domElement.setAttribute(prop, reactElement.props[prop])
+    
+  }
+  containor.appendChild(domElement)
 }
 
 const reactElement = {
