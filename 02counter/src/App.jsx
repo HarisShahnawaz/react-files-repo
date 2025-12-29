442018964ffe39400d1e15cedb623bf2303b6bd5
HarisShahnawaz <harisshahnawaz97@gmail.com>
2025-12-29 23:40:29 +0500

    update

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -1,23 +1,32 @@
+
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const addvalue = () => {
-    console.log("clicked", Math.random());
-    counter = counter + 1;
-    
+  const [counter, setCounter] = useState(15)
+
+  const addValue = () => {
+    if (counter < 20) {
+      setCounter(counter + 1)
+    }
   }
- let counter =  15
+
+  const removeValue = () => {
+    if (counter > 0) {
+      setCounter(counter - 1)
+    }
+  }
+
   return (
     <>
-  <h1>React Learning Program</h1>
-  <h2>Counter value : {counter}</h2>
-  <button onClick={addvalue}> Add Value </button>
-  <br />
-  <button>Remove value</button>
-   
+      <h1>React Learning Program</h1>
+      <h2>Counter value : {counter}</h2>
+      <button onClick={addValue}> Add Value {counter} </button>
+      <br />
+      <button onClick={removeValue}>Remove value {counter}</button>
+      <p> {counter}</p>
     </>
   )
 }
