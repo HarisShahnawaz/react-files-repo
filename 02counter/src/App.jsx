f79813cd33cac57ac46f711f7740dfb5c362b5a5
HarisShahnawaz <harisshahnawaz97@gmail.com>
2026-01-03 23:04:44 +0500

    update

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   const [counter, setCounter] = useState(15)
 
   const addValue = () => {
-    if (counter < 40) {
+    if (counter < 60) {
       setCounter(counter + 1)
     }
   }
