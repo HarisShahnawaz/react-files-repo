b209bb4bc1ccff66a125bbb1fb02c2fe72058b63
Haris Shahnawaz <harisshahnawaz97@gmail.com>
2026-02-14 18:09:34 +0500

    1st project for context api

--- 08mini-context/src/App.jsx
+++ 08mini-context/src/App.jsx
@@ -1,15 +1,16 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import UserContextProvider from './context/UserContextProvider';  
 import './App.css'
 
 function App() {
   
 
   return (
-    <>
+    <UserContextProvider>
       <h1>react with harry</h1>
-    </>
+    </UserContextProvider>
   )
 }
 
--- 08mini-context/src/context/userContext.js
+++ 08mini-context/src/context/userContext.js
@@ -1,1 +1,5 @@
-import React from 'react'
+import React, { createContext } from 'react'
+
+ const UserContext = React.createContext();
+
+ export default UserContext;
