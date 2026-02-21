b89919ff174fc018144f6668d31cf90b5bc1519e
Haris Shahnawaz <harisshahnawaz97@gmail.com>
2026-02-21 23:25:51 +0500

    create button and input file in megaproject

--- 12MegaBlog/src/components/Header/Header.jsx
+++ 12MegaBlog/src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ import { useNavigate } from "react-router-dom";
 function Header() {
     const authStatus = useSelector((state) => state.auth.status);
     const navigate = useNavigate();
-    const naitems =[
+    const navItems =[
         {
             name: "Home",
             slug: "/",
@@ -40,9 +40,25 @@ function Header() {
     <nav className="flex">
         <div className="mr-4">
         <Link to="/">
-       <Logo />
+       <Logo width="70px" />
+       
         </Link>
         </div>
+       <ul className="flex ml-auto">
+         {navItems.map((item) => 
+         item.active ? (
+            <li key={item.name}>
+                <button
+                onClick={() => navigate(item.slug)}
+                className="inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full">{item.name}</button>
+            </li>
+         ): null
+        )}
+        {authStatus && <LogoutBtn />}
+        <li>
+            <LogoutBtn/>
+        </li>
+       </ul>
     </nav>
    </Container>
    </header>
