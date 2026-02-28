ebdf4cb9db385ec68fcd456a9554bae9fc8dd996
Haris Shahnawaz <harisshahnawaz97@gmail.com>
2026-02-28 23:34:33 +0500

    FIX BUGS IN THESE FILES

--- 12MegaBlog/src/components/Input.jsx
+++ 12MegaBlog/src/components/Input.jsx
@@ -1,26 +1,38 @@
-import React from 'react'
-import { useId } from 'react'
+import React, { useId } from "react";
 
-const Input = React.forwardRef(function Input({
-        label,
-        type = "text",
-        className = "",
-        ...props
-},ref) {
-   id = useId();
-    return 
-    <div className='w-full'>
-        {label && <label 
-        className='inline-block mb-1 pl-1'  htmlFor={id}>{label}</label>}
-       <input
-             type={type}
-             className={` px-3 py-2 border rounded-lg bg-white text-black 
-                outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
-             ref={ref}
-             {...props}
-             id={id}
-       />
+const Input = React.forwardRef(function Input(
+  {
+    label,
+    type = "text",
+    className = "",
+    ...props
+  },
+  ref
+) {
+  const id = useId(); // ✅ FIXED
+
+  return (
+    <div className="w-full">
+      {label && (
+        <label
+          className="inline-block mb-1 pl-1"
+          htmlFor={id}
+        >
+          {label}
+        </label>
+      )}
+
+      <input
+        id={id}
+        type={type}
+        ref={ref}
+        className={`px-3 py-2 border rounded-lg bg-white text-black
+        outline-none focus:bg-gray-50 duration-200
+        border-gray-200 w-full ${className}`}
+        {...props}
+      />
     </div>
-})
+  );
+});
 
-export default Input
+export default Input;
--- 12MegaBlog/src/pages/AddPosts.jsx
+++ 12MegaBlog/src/pages/AddPosts.jsx
@@ -11,4 +11,4 @@ function AddPost() {
   )
 }
 
-export default AddPosts
+export default AddPost
